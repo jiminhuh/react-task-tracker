b053e4b2506a35131cfb95ac32bd1ff6d2fe0471
Jimin Huh <jiminhuh95@gmail.com>
2021-02-01 15:33:42 -0600

    finished UI components, created prod build

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
-
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -52,8 +52,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
-      <AddTask onAdd={addTask}/>
+      <Header title="Task Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Tasks to Show"}
     </div>
   );
